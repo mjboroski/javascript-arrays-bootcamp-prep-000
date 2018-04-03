569b6ca0a0edb8cd8bd7534e7d08e7089df26594
Michael Boroski <mjboroski@gmail.com>
2018-04-03 15:56:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,5 +25,5 @@ function removeElementFromBeginningOfArray(array){
   return array.slice(1);
 }
 function destructivelyRemoveElementFromEndOfArray(array){
- return array.slice(array.length - 1);
+ return array.slice(-1);
 }
