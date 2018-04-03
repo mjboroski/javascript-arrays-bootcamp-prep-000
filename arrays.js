af8a95f0e999cf38250b7a4675ced769ad01b181
Michael Boroski <mjboroski@gmail.com>
2018-04-03 15:37:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,7 +4,7 @@ function addElementToBeginningOfArray(array, element){
   return [element, ...array];
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array[0]=element;
+  array=array.unshift(element)
   return array;
 }
 function addElementToEndOfArray (array, element){
